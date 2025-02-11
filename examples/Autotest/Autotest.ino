7f48e0db3a8d887be3b89c8499e3f632ec2c870d
Catalina Huincahue <chuincahue@rebornelectric.cl>
2025-02-11 08:56:23 -0300

    Actualización: código autotest exitoso
    Testeo pin a pin de todas las placas de segunda etapa.

--- examples/Autotest/Autotest.ino
+++ examples/Autotest/Autotest.ino
@@ -1,30 +1,51 @@
-#include <MUX.h>
-// Este es un "example" disponible para correr en IDE de Arduino
-// Desde Visual Studio marca error en #include <MUX.h> pero en IDE funciona perfecto 
+//---------------------------------------------------//
+//---------[Autotest - Tester de Ramales V2]---------//
+//--------Testeo entre pines de segunda etapa--------//
+//---------------------------------------------------//
+
+// Testeo automático del pin de un canal consigo mismo 
+// y el resto de pines de ese canal.
+// Para todos los canales de todas las placas de
+// primera etapa.
+
+#include <MUX_I2C_PCF8574.h>
+#include <Wire.h>
+
 
 void setup() {
- Serial.begin(9600);
-
+  Serial.begin(115200);
 }
 
 void loop() {
-MUX myMUX;
-  for (uint16_t i = 0x0000; i < 0x0400; i++) 
-  {
-     //for (uint16_t j = 0; j < 1024; j++) {
-        Serial.print("Testing PIN_A: ");
-        Serial.print(i,HEX);
-        Serial.print("     with PINB:");
-        Serial.print(i,HEX);
-        Serial.print("     RESULT:   ");
-        if(myMUX.test(i,i ))
-        {
-          Serial.println("Buenolino");
+  I2C_MUX_SYSYEM MyMegaMux;
+  Wire.begin();
+  Serial.println(">>>>>>>>>>>>>>>>>>>> BEGIN AUTOTEST <<<<<<<<<<<<<<<<<<<<<");
+  Serial.println("           ");
+
+    for (int i = 0; i < 4; i++) //--------Outer loop largo 4
+    { 
+      Serial.print("M");
+      Serial.println(i); 
+      Serial.println();
+       
+      for (int j = 0; j < 16; j++) //-----Middle loop largo 16
+      {    
+        for (int k = 0; k < 16; k++) //---Inner loop largo 16
+        { 
+          Serial.print(MyMegaMux.is_conected((i<<8)|(j<<4)|k,(i<<8)|(j<<4)|k));
+          //delay(5);
         }
-        else{
-          Serial.println("Malolino");
-        }
-      //}      
-  }
-  while(1);
+        Serial.println();        
+      }
+      Serial.println();  
+    }
+  Serial.println(">>>>>>>>>>>>>>>>>>>> END AUTOTEST <<<<<<<<<<<<<<<<<<<<<");
+  Serial.println("           ");
+  while(2); //para detener el proceso
 }
+
+ 
+
+
+
+
